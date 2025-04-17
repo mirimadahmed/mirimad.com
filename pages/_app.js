fbaba08602b2b24582710090f3d601bac8099ba5
Mir Imad Ahmed <mirimadahmed@outlook.com>
2025-04-17 23:31:29 +0800

    ga added

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const App = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <GoogleAnalytics gaId="G-34RN3QNNBT" />
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 };
 
